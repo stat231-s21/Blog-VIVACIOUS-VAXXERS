c76105f1dd3e958b4991366b34c72b98da952905
Brandon Kwon <bkwon23@amherst.edu>
2021-05-10 14:40:51 -0700

    committing mental health stuff

--- data_wrangling/data_wrangling_brandon.R
+++ data_wrangling/data_wrangling_brandon.R
@@ -40,3 +40,43 @@ HT_race_private_L <- HT_race_private %>%
 							names_sep = "_", 
 							values_from = c("Value", "Low CI", "High CI", "Confidence Interval")) %>%
 	mutate(Value_1 = readr::parse_number(as.character(Value_1)))
+
+
+
+Access_of_care <- read_csv("data/Indicators_of_Reduced_Access_to_Care_Due_to_the_Coronavirus_Pandemic_During_Last_4_Weeks.csv")
+
+#splitting Access_of_care_race dataset by Indicator
+Access_of_care_race <- Access_of_care %>% 
+	filter(Group == "By Race/Hispanic ethnicity") %>% 
+	select(-Group, -State, -Phase, -'Time Period Label', -'Time Period Start Date', -'Time Period End Date')
+Access_of_care_race_delayed <- Acess_of_care_race %>%
+	filter(Indicator == "Delayed Medical Care, Last 4 Weeks") %>%
+	select(-Indicator, -'Quartile Range')
+Access_of_care_race_did_not <- Access_of_care_race %>%
+	filter(Indicator == "Did Not Get Needed Care, Last 4 Weeks")
+Access_of_care_race_both <- Access_of_care_race %>%
+	filter(Indicator == "Delayed or Did Not Get Care, Last 4 Weeks")
+
+#formatting Access_of_care_race_delayed into wide format
+Access_of_care_race_delayed_L <- Access_of_care_race_delayed %>%
+	pivot_wider(id_cols = Subgroup, 
+							names_from = "Time Period", 
+							names_sep = "_", 
+							values_from = c("Value", "Low CI", "High CI", "Confidence Interval")) %>%
+	mutate(Value_1 = readr::parse_number(as.character(Value_1)))
+
+#formatting Access_of_care_race_did_not into wide format
+Access_of_care_race_did_not_L <- Access_of_care_race_did_not %>%
+	pivot_wider(id_cols = Subgroup, 
+							names_from = "Time Period", 
+							names_sep = "_", 
+							values_from = c("Value", "Low CI", "High CI", "Confidence Interval")) %>%
+	mutate(Value_1 = readr::parse_number(as.character(Value_1)))
+
+#formatting Access_of_care_race_both into wide format
+Access_of_care_race_both_L <- Access_of_care_race_both %>%
+	pivot_wider(id_cols = Subgroup, 
+							names_from = "Time Period", 
+							names_sep = "_", 
+							values_from = c("Value", "Low CI", "High CI", "Confidence Interval")) %>%
+	mutate(Value_1 = readr::parse_number(as.character(Value_1)))
